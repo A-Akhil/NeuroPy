27ba34c9b67efc8e307272581c611796c32719c6
az3805 <az3805@srmist.edu.in>
2024-03-15 21:31:29 +0530

    Changes in test.py

--- NeuroPy/test.py
+++ NeuroPy/test.py
@@ -4,27 +4,26 @@ import os
 import time
 from tqdm import tqdm
 
-
 att = tqdm(total=100, desc="attention")
 med = tqdm(total=100, desc="meditation")
 
-object1=NeuroPy("/dev/ttyUSB0", 115200, '7d55')
+object1 = NeuroPy("/dev/rfcomm0", 115200, '7d55')
 
-volume = [ 50, 50, 50, 50, 50]
+volume = [50, 50, 50, 50, 50]
+
 
 def attention_callback(value):
     "this function will be called everytime NeuroPy has a new value for attention"
-#    os.system("amixer sset 'Master' " + str(value) + "% > /dev/null")
-#    att.moveto(value)
+    #    os.system("amixer sset 'Master' " + str(value) + "% > /dev/null")
+    #    att.moveto(value)
 
-#    print
-    #do other stuff (fire a rocket), based on the obtained value of attention_value
-    #do some more stuff
+    #    print
+    # do other stuff (fire a rocket), based on the obtained value of attention_value
+    # do some more stuff
     return None
 
 
 def meditation_callback(value):
-
     volume.append(value)
     volume.pop(0)
     med.update(value)
@@ -35,19 +34,22 @@ def meditation_callback(value):
         value = value + v
     value = value / len(volume)
 
-    os.system("amixer sset 'Master' " + str(value) + "% > /dev/null")
-    #do other stuff (fire a rocket), based on the obtained value of attention_value
-    #do some more stuff
+    # os.system("amixer sset 'Master' " + str(value) + "% > /dev/null")
+    os.system("amixer sset 'PCM' " + str(value) + "% > /dev/null")
+
+    # do other stuff (fire a rocket), based on the obtained value of attention_value
+    # do some more stuff
     return None
 
 def blink_cb(value):
-    print "Blink ", value
+    print("Blink ", value)
 
-#set call back:
-object1.setCallBack("attention",attention_callback)
-object1.setCallBack("meditation",meditation_callback)
-object1.setCallBack("blinkStrength",blink_cb)
-#call start method
+
+# set call back:
+object1.setCallBack("attention", attention_callback)
+object1.setCallBack("meditation", meditation_callback)
+object1.setCallBack("blinkStrength", blink_cb)
+# call start method
 object1.start()
 
 try:
